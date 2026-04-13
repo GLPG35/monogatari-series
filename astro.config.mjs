e3e3af01a5babf6c7e102523d951a6123d2a96a5
Gian Luca <55563130+GLPG35@users.noreply.github.com>
2026-04-12 22:25:41 -0300

    v0.1.0
    ADd robots.txt
    
    Fix 404 page for mobile devices

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,17 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import robotsTxt from 'astro-robots-txt';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  site: 'https://monogatari-series.vercel.app/',
+  integrations: [react(), robotsTxt({
+    policy: [
+      {
+        userAgent: '*',
+        disallow: ''
+      }
+    ]
+  })]
 });
