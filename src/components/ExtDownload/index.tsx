d6fcf4569a48226b0bf9ae8456b4226f14663235
Gian Luca <55563130+GLPG35@users.noreply.github.com>
2026-04-27 13:32:17 -0300

    v0.6.7
    Add loading indicator for direct download links

--- src/components/ExtDownload/index.tsx
+++ src/components/ExtDownload/index.tsx
@@ -1,7 +1,11 @@
-import type { PropsWithChildren } from 'react'
+import { useState, type PropsWithChildren } from 'react'
+import './styles.scss'
 
 const ExtDownload = ({ href, name, children }: PropsWithChildren<{ href: string, name: string }>) => {
+	const [loading, setLoading] = useState(false)
+	
 	const handleClick = async () => {
+		setLoading(true)
 		const res = await fetch(href)
 		const blob = await res.blob()
 		const blobURL = URL.createObjectURL(blob)
@@ -12,12 +16,18 @@ const ExtDownload = ({ href, name, children }: PropsWithChildren<{ href: string,
 		link.click()
 
 		URL.revokeObjectURL(blobURL)
+		setLoading(false)
 	}
 	
 	return (
-		<div className='anchor' onClick={handleClick}>
-			{children}
-		</div>
+		<>
+			<div className='anchor' onClick={handleClick}>
+				{children}
+			</div>
+			{loading && 
+				<span className="loader"></span>
+			}
+		</>
 	)
 }
 
