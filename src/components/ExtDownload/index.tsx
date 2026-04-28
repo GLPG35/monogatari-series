5350e40ffb0ca837912d094cfcb1a40f6560596f
Gian Luca <55563130+GLPG35@users.noreply.github.com>
2026-04-28 08:51:58 -0300

    v0.6.8
    Add percentage indicator to external downloads
    
    Add mirror links to light novel downloads page

--- src/components/ExtDownload/index.tsx
+++ src/components/ExtDownload/index.tsx
@@ -3,11 +3,40 @@ import './styles.scss'
 
 const ExtDownload = ({ href, name, children }: PropsWithChildren<{ href: string, name: string }>) => {
 	const [loading, setLoading] = useState(false)
+	const [percentage, setPercentage] = useState(0)
 	
 	const handleClick = async () => {
 		setLoading(true)
 		const res = await fetch(href)
-		const blob = await res.blob()
+
+		const totalSize = parseInt(res.headers.get('content-length') as string, 10)
+
+		if (!res.body) return
+
+		const reader = res.body.getReader()
+		let receivedSize = 0
+		let lastReportedProgress = 0
+		const chunks = []
+
+		while (true) {
+			const { done, value } = await reader.read()
+			
+			if (done) break
+
+			chunks.push(value)
+			receivedSize += value.length
+
+			if (totalSize) {
+				const currentProgress = Math.floor((receivedSize / totalSize) * 100)
+
+				if (currentProgress > lastReportedProgress) {
+					lastReportedProgress = currentProgress
+					setPercentage(() => currentProgress)
+				}
+			}
+		}
+		
+		const blob = new Blob(chunks)
 		const blobURL = URL.createObjectURL(blob)
 
 		const link = document.createElement('a')
@@ -25,7 +54,10 @@ const ExtDownload = ({ href, name, children }: PropsWithChildren<{ href: string,
 				{children}
 			</div>
 			{loading && 
-				<span className="loader"></span>
+				<>
+					<span className="loader"></span>
+					<span className='percentage'>{percentage}%</span>
+				</>
 			}
 		</>
 	)
